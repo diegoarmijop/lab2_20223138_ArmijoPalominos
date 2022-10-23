bbb2b846b22e5bcdee3fe3f3fa4def750e3ea17d
unknown <diego.armijo.p@usach.cl>
2022-10-23 19:05:35 -0300

    Se definio una estructura en el codigo. Se agrego funcion imageFlipH e imageFlipV (funcionando al 100%). Se agrego imageCrop (falta asignar las nuevas coords).

--- Laboratorio-2-Prolog.pl
+++ Laboratorio-2-Prolog.pl
@@ -7,71 +7,32 @@
 %Dominio: X(int) x Y(int) x Bit (0 | 1) x Depth(int). 
 % X e Y representan la posicion del pixel, bit su color y Depth la profundidad del pixel. 
 
-pixbit(X,Y,Bit,Depth,[[X,Y],Bit,Depth,"pixbit", 1]):-
+%Constructor pixbit.
+pixbit(X,Y,Bit,Depth,[X,Y,Bit,Depth,"pixbit"]).
+
+%Funcion de pertenencia de pixbit.
+verifyPixbit(Pix):-
+    pixbit(X,Y,Bit, Depth, Pix),
     integer(X), 
     X >= 0, 
     integer(Y),
     Y >= 0,
     integer(Bit),
-    Bit = 0,
+    Bit >= 0,
+    Bit < 2,
     integer(Depth), 
     Depth >= 0,!.
-
-pixbit(X,Y,Bit,Depth,[[X,Y],Bit,Depth,"pixbit", 0]):-
-    integer(X), 
-    X >= 0, 
-    integer(Y),
-    Y >= 0,
-    integer(Bit),
-    Bit = 1,
-    integer(Depth), 
-    Depth >= 0,!.
-
-transform1(Num, Num1):-
-    NumAux is floor(Num/16),
-    NumAux >= 0,
-    NumAux < 10, 
-    Num1 is NumAux,!. 
-
-transform1(Num, Num1):-
-    NumAux is floor(Num/16),
-    NumAux = 10,
-    Num1 is "A",!. 
-
-transform1(Num, Num1):-
-    NumAux is floor(Num/16),
-    NumAux = 11,
-    Num1 is "B",!. 
-
-transform1(Num, Num1):-
-    NumAux is floor(Num/16),
-    NumAux = 12,
-    Num1 is "C",!. 
-
-transform1(Num, Num1):-
-    NumAux is floor(Num/16),
-    NumAux = 13,
-    Num1 is "D",!.
-
-transform1(Num, Num1):-
-    NumAux is floor(Num/16),
-    NumAux = 14,
-    Num1 is "E",!. 
-
-transform1(Num, Num1):-
-    NumAux is floor(Num/16),
-    NumAux = 15,
-    Num1 is "F",!. 
-
-transform2(Num, Num1):-
-    Num1 is ((Num/16) - floor(Num/16))*16.
-    
 
 %TDA pixrgb
 %Dominio: X(int) x Y(int) x R(C) x G(C) x B(C) x Depth(int). 
 % X e Y representan la posicion del pixel, R G B los colores y Depth la profundidad.
 
-pixrgb(X,Y,R,G,B,Depth,[X,Y,[R,G,B],Depth,"pixrgb"]):-
+%Constructor de pixrgb.
+pixrgb(X,Y,R,G,B,Depth,[X,Y,[R,G,B],Depth,"pixrgb"]).
+
+%Funcion de pertenencia de pixrgb.
+verifyPixrgb(Pix):-
+    pixrgb(X,Y,R,G,B,Depth,Pix),
     integer(X),
     X >= 0, 
     integer(Y),
@@ -87,28 +48,23 @@ pixrgb(X,Y,R,G,B,Depth,[X,Y,[R,G,B],Depth,"pixrgb"]):-
     B < 256,
     integer(Depth),
     Depth >= 0.
-    %transform1(R, R1), 
-    %transform2(R, R2),
-    %transform1(G, G1), 
-    %transform2(G, G2),
-    %transform1(B, B1), 
-    %transform2(B, B2).
-    
-%Funcion que determina si un elemento pertenece a una lista. 
-member(X,[X|_]),!.
-member(X,[_|Xs]) :- member(X,Xs),!.
-
-%Funcion que determina si un numero es hexadecimal.
-esHex([]).
-esHex([Cabeza|Cola]):-
-    member(Cabeza, [35,48,49,50,51,52,53,54,55,56,57,65,66,67,68,69,70]),!,
-    esHex(Cola).    
 
 %TDA pixhex
 %Dominio: X(int) x Y(int) x Hex(string) x D(int). S
 % X e Y representan las coordenadas del pixel, Hex su color en hexadecimal y D la profundidad. 
 
-pixhex(X,Y,Hex,D,[[X,Y],Hex,D,"pixhex"]):-
+%Constructor de pixhex.
+pixhex(X,Y,Hex,D,[X,Y,Hex,D,"pixhex"]).
+
+%Funcion que determina si un numero es hexadecimal.
+esHex([]).
+esHex([Cabeza|Cola]):-
+    member(Cabeza, [35,48,49,50,51,52,53,54,55,56,57,65,66,67,68,69,70]),!,
+    esHex(Cola). 
+
+%Funcion de pertenencia de pixhex.
+verifyPixhex(Pix):-
+    pixhex(X,Y,Hex,D,Pix),
     integer(X), 
     X >= 0,
     integer(Y), 
@@ -116,154 +72,199 @@ pixhex(X,Y,Hex,D,[[X,Y],Hex,D,"pixhex"]):-
     string(Hex), 
     string_length(Hex, L), 
     L = 7, 
-    string_to_list(Hex, Aux1),
-    esHex(Aux1).
+    string_to_list(Hex, L1),
+    esHex(L1),
+    integer(D),
+    D >= 0.
 
 %TDAimage 
-image(Width,Height,Pix,[Width,Height,Pix,L]):-
+%Dominio: Width(int) x Height(int) x Pixeles(string) x D(int). S
+% Width x Height representan el ancho y alto de una fotografia... 
+
+%Constructor de image.
+image(Width,Height,Pixels,[Width,Height,Pixels,TypePixels]):-
+    maplist(getTypePixels, Pixels, TypePixels).
+
+%Funcion de pertenencia de image.
+verifyImage(Image):-
+    image(Width,Height,Pixels,Image),
     integer(Width), 
     Width >= 0,
     integer(Height),
     Height >= 0,
-    is_list(Pix),
-    WxH is Width*Height,
-    length(Pix, WxH),
-    maplist(getTypePix, Pix, L).
-    %pack(L,L2),!.
-
-
-getTypePix([_,_,_,Type|_], Type).
-
-getListOfListPix([_,_,_,TypePix|_], TypePix).
+    is_list(Pixels),
+    WidthXHeight is Width*Height,
+    length(Pixels, WidthXHeight).
 
 %Funcion que verifica si la lista contiene solo "pixbit".
-verificarPixbit([]).
-verificarPixbit([Cabeza|Cola]):-
+isPixbit([]).
+isPixbit([Cabeza|Cola]):-
     equals(Cabeza, "pixbit"),!,
-    verificarPixbit(Cola).
+    isPixbit(Cola).
 
 %Funcion que verifica si una imagen es bitmap.
 imageIsBitmap(Image):-
-    getListOfListPix(Image, L1),
-    %getFirstElement(L1, L), 
-    verificarPixbit(L1),!, 
-    write("true").
+    verifyImage(Image),
+    getListPixels(Image, Aux),
+    isPixbit(Aux),!.
 
 %Funcion que verifica si la lista contiene solo "pixrgb".
-verificarPixmap([]).
-verificarPixmap([Cabeza|Cola]):-
+isPixmap([]).
+isPixmap([Cabeza|Cola]):-
     equals(Cabeza, "pixrgb"),!,
-    verificarPixmap(Cola).
+    isPixmap(Cola).
 
 %Verifica si la imagen es un Pixmap.
 imageIsPixmap(Image):-
-    getListOfListPix(Image, L1), 
-    getFirstElement(L1, L),
-    verificarPixmap(L), 
-    write("true").
+    verifyImage(Image),
+    getListPixels(Image, Aux),
+    isPixmap(Aux). 
 
 %Funcion que verifica si la lista contienen solo "pixhex".
-verificarHexmap([]).
-verificarHexmap([Cabeza|Cola]):-
+isHexmap([]).
+isHexmap([Cabeza|Cola]):-
     equals(Cabeza, "pixhex"),!,
-    verificarHexmap(Cola).
+    isHexmap(Cola).
 
 %Verifica si la imagen es un Hexmap.
 imageIsHexmap(Image):-
-    getListOfListPix(Image, L1), 
-    getFirstElement(L1, L),
-    verificarHexmap(L), 
-    write("true").
+    verifyImage(Image),
+    getListPixels(Image, Aux),
+    isHexmap(Aux).
 
-%Obtiene el primer elemento de una lista.
-getFirstElement([Cabeza|_], Cabeza). 
+%Funcion que encuentra el numero de elementos de una lista.
+listLength([],0).
+listLength([_|L],N) :- listLength(L,N1), N is N1 + 1.
+
+%Funcion que verifica si una imagen esta comprimida.
+%imageIsCompressed
+imageIsCompressed(Image):-
+    image(Width,Height,Pixels,Image),
+    WidthXHeight is Width*Height,
+    listLength(Pixels, Count),
+    Count \= WidthXHeight.
+
+addElement(Element, [], [Element]).
+addElement(Element, List, [Element|List]).
+
+%Funcion que invierte una imágen horizontalmente.
+imageFlipH(Image, NewImage):-
+    image(Width,Height,Pixels, Image),
+    pixelIsFlipH(Pixels, Height, NewPixels),
+    sort(NewPixels, NewPixelsO),
+    image(Width, Height, NewPixelsO, NewImage).
     
-%Compara que dos numeros sean iguales.
-equals(X, Y):- X = Y.
+%Funcion que modifica la coordenada Y en una lista de pixeles dependiendo del ancho de una imagen.
+pixelIsFlipH([],_,[]).
+pixelIsFlipH([Pixel|Resto], Ancho, NewPixeles):-
+    pixelIsFlipH(Resto, Ancho, AuxNewPixeles),
+    (   verifyPixbit(Pixel) 
+    ->  pixbit(X,Y,Bit,Depth, Pixel);
+    verifyPixrgb(Pixel)
+    -> pixrgb(X,Y,R,G,B,Depth,Pixel);
+    verifyPixhex(Pixel)
+    ->  pixhex(X,Y,Hex,D, Pixel)),
+    NewY is abs(Y - (Ancho-1)),
+    (   verifyPixbit(Pixel) 
+    ->  pixbit(X,NewY,Bit,Depth, NewPixel);
+    verifyPixrgb(Pixel)
+    -> pixrgb(X,NewY,R,G,B,Depth,NewPixel);
+    verifyPixhex(Pixel)
+    ->  pixhex(X,NewY,Hex,D, NewPixel)),
+    addElement(NewPixel, AuxNewPixeles, NewPixeles).
 
-%Flip-H
-getListOfPix([_,_,Pix|_],Pix).
-%maplist(getFirstElement, ListPix, L).
+%Funcion que invierte una imágen verticalmente.
+imageFlipV(Image, NewImage):-
+    image(Width,Height,Pixeles, Image),
+    pixelIsFlipV(Pixeles, Width, NewPixels),
+    sort(NewPixels, NewPixelesO),
+    image(Width, Height, NewPixelesO, NewImage).
+
+%Funcion que modifica la coordenada X en una lista de pixeles dependiendo del alto de una imagen.
+pixelIsFlipV([],_,[]).
+pixelIsFlipV([Pixel|Resto], Alto, NewPixeles):-
+    pixelIsFlipV(Resto, Alto, AuxNewPixeles),
+    (   verifyPixbit(Pixel) 
+    ->  pixbit(X,Y,Bit,Depth, Pixel);
+    verifyPixrgb(Pixel)
+    -> pixrgb(X,Y,R,G,B,Depth,Pixel);
+    verifyPixhex(Pixel)
+    ->  pixhex(X,Y,Hex,D, Pixel)),
+    NewX is abs(X - (Alto-1)),
+    (   verifyPixbit(Pixel) 
+    ->  pixbit(NewX,Y,Bit,Depth,NewPixel);
+    verifyPixrgb(Pixel)
+    -> pixrgb(NewX,Y,R,G,B,Depth,NewPixel);
+    verifyPixhex(Pixel)
+    ->  pixhex(NewX,Y,Hex,D, NewPixel)),
+    addElement(NewPixel, AuxNewPixeles, NewPixeles).
+    
+
+%Funcion que recorta una imágen a partir de un cuadrante.
+%Falta cambiar Newcoords.
+imageCrop(Image,X1,Y1,X2,Y2, NewImage):-
+    image(_,_,Pixeles,Image),
+    crop(Pixeles,X1,Y1,X2,Y2,NewPixels),
+    maxElement(X1,X2,X3),
+    maxElement(Y1,Y2,Y3),
+    NewWidth is Y3 + 1,
+    NewHeight is X3 + 1,
+    image(NewWidth, NewHeight, NewPixels, NewImage).
+
+%Funcion que dada una lista de pixeles, esta va agregando los pixeles que 
+%se encuentren dentro de un rango dado.
+crop([],_,_,_,_,[]).
+crop([Pixel|Resto],X1,Y1,X2,Y2,NewPixeles):-
+    crop(Resto,X1,Y1,X2,Y2,AuxNewPixeles),
+    (   verifyPixbit(Pixel) 
+    ->  pixbit(X,Y,_,_, Pixel);
+    verifyPixrgb(Pixel)
+    -> pixrgb(X,Y,_,_,_,_,Pixel);
+    verifyPixhex(Pixel)
+    ->  pixhex(X,Y,_,_,Pixel)),
+    %ordenar x1 y1
+    (   filterCropX(X,X1,X2), filterCropY(Y,Y1,Y2)
+    ->  addElement(Pixel, AuxNewPixeles, NewPixeles)
+    ;   NewPixeles = AuxNewPixeles).
+
+%Filtro para verificar que la coord X se encuentre dentro de un rango.
+filterCropX(X,X1,X2):-
+    (X>=X1, X =< X2).
+
+%Filtro para verificar que la coord Y se encuentre dentro de un rango.
+filterCropY(Y,Y1,Y2):-
+    (Y >=Y1, Y =< Y2).
+
+%Funcion que me devuelve el numero mayor entre dos numeros.
+maxElement(X1,X2,X3):-
+    (X1 >= X2
+    ->  X3 is X1;
+    X3 is X2
+    ).
+
+%Funcion que determina si un elemento pertenece a una lista. 
+member(X,[X|_]).
+member(X,[_|Xs]) :- member(X,Xs),!.
 
 
-%Funciones test.
+getTypePixels([_,_,_,_,Type|_], Type).
+getListPixels1([_,_,Pixels|_], Pixels).
+getListPixels([_,_,_,TypePix|_], TypePix).
 
-is_list([]).
-is_list([_|T]) :-
-        is_list(T).
-
-getPix([Cabeza|_], Cabeza).
-
-
-succ(X, Y) :- Y is X + 1.
 maplist(_,[],[]).
 maplist(P,[A|As],[B|Bs]) :- call(P,A,B),
 maplist(P,As,Bs),!.
 
-
-are_identical(X, Y) :-
-    X == Y.
-
-%Funcion que filtra una lista. (No se puede utilizar).
-filterList(A, In, Out) :-
-    exclude(are_identical(A), In, Out).
- 
-%No Funciona    
-slice([X|_],1,1,[X]).
-slice([X|Xs],1,K,[X|Ys]) :- K > 1, K1 is K - 1, slice(Xs,1,K1,Ys). 
-slice([_|Xs],I,K,Ys) :- I > 1, I1 is I - 1, K1 is K - 1, slice(Xs,I1,K1,Ys).
-
-remover( _, [], []).
-remover( R, [R|T], T).
-remover( R, [H|T], [H|T2]) :- H \= R, remover(R, T, T2).
-
-
-verifyX(X, Y):-
-    getFirstElement(X, X1),
-    getFirstElement(Y, Y1),
-     X1 \= Y1.
-
-pack([],[]).
-pack([X|Xs],[Z|Zs]) :- transfer(X,Xs,Ys,Z), pack(Ys,Zs).
-
-transfer(X,[],[],[X]).
-transfer(X,[Y|Ys],[Y|Ys],[X]) :- getFirstElement(X, L1), getFirstElement(Y, L2), L1 \= L2.
-transfer(X,[X|Xs],Ys,[X|Zs]) :- transfer(X,Xs,Ys,Zs).
-
-getSecondElement([_,Element|_], Element). 
-
-split(L,0,[],L).
-split([X|Xs],N,[X|Ys],Zs) :- N > 0, N1 is N - 1, split(Xs,N1,Ys,Zs).
-
-%Intento de implementar flip de otra manera. 
-
-
-%Meta: Hacer funcion que reciba 
-
-FlipH(Image, NewImage):-
-    image(Width,Height,Pix, Image),
-    pixelIsFlipH(Pix, Height, NewPixels),
-    image(Width, Height, NewPixels, NewImage).
-
-pixelIsFlipH([Pixel|Resto], Ancho, NewPixeles):-
-    pixbit(X,Y,Bit,Depth, Pixel),
-    ( Y < Ancho
-    -> NewY is Y + 1
-    ; NewY is Y - 1
-    ),
-    pixbit(X, NewY, Bit, Depth, NewPixel).
-    agregar(NewPixel, NewPixels).
-
-agregar(Elemento, [], [Elemento]).
-agregar(Elemento, Lista, [Elemento|Lista]).
+equals(X, Y):- X = Y.
 
 
 
-
-
-
-
-
-%Image de prueba. 
 %pixrgb( 0, 0, 1, 30, 40, 10, P1), pixrgb( 0, 1, 1, 10, 10, 10, P2), pixrgb( 1, 0, 1, 20, 20, 20, P3), pixrgb( 1, 1, 1, 70, 40, 10, P4), image(2,2,[P1,P2,P3,P4],CS), imageIsBitmap(CS).
 %pixbit(0, 0, 1,10, P1), pixbit(0, 1, 0,20, P2),pixbit(1, 0, 1,30, P3),pixbit(1, 1, 1,40, P4), image(2,2, [P1,P2,P3,P4], CS).
+%pixrgb( 0, 0, 1, 30, 40, 10, P1).
+
+/** <examples>
+?- 
+pixbit(0, 0, 1,10, P1), pixbit(0, 1, 0,20, P2),pixbit(1, 0, 1,30, P3),pixbit(1, 1, 1,40, P4), image(2,2, [P1,P2,P3,P4], CS), verifyImage(CS), imageIsBitmap(CS).
+
+*/
